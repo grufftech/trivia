0ce300919f6b099fdea2af647f4b15fb5e25a584
Adam Baird <adam.baird@gmail.com>
2020-04-09 16:09:34 -0700

    button cleanup

--- resources/views/round/form.blade.php
+++ resources/views/round/form.blade.php
@@ -4,5 +4,5 @@
  {!! Form::hidden('game_id', $game->id); !!}
 </div>
 <div class='form-group'>
- {!! Form::submit($submitButtonText, ['class' => 'btn btn-lg btn-success form-control']) !!}
+ {!! Form::submit($submitButtonText, ['class' => 'btn btn-sm btn-primary form-control']) !!}
 </div>
