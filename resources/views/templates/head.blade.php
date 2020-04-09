d494e9c150a303090e5e33089a0f05e98491853b
Adam Baird <adam.baird@gmail.com>
2020-04-09 13:39:30 -0700

    updated share ref

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -1,23 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="title" content="Pixel Lite - Open Source Bootstrap UI Kit">
-<meta name="author" content="Themesberg">
-<meta name="description" content="Open source and accessibility oriented Bootstrap UI Kit featuring over 80 premium components and 4 example pages.">
-<meta name="keywords" content="bootstrap, bootstrap ui kit, accessiblity, open source, open source bootstrap ui kit" />
+<meta name="title" content="Online Trivia">
+<meta name="author" content="">
 <link rel="canonical" href="https://themesberg.com/product/ui-kits/pixel-lite-free-bootstrap-4-ui-kit">
-
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://demo.themesberg.com/pixel-lite">
-<meta property="og:title" content="Pixel Lite - Open Source Bootstrap UI Kit">
-<meta property="og:description" content="Open source and accessibility oriented Bootstrap UI Kit featuring over 80 premium components and 4 example pages.">
-<meta property="og:image" content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/pixel-lite/pixel-lite-preview.jpg">
-
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://demo.themesberg.com/pixel-lite">
-<meta property="twitter:title" content="Pixel Lite - Open Source Bootstrap UI Kit">
-<meta property="twitter:description" content="Open source and accessibility oriented Bootstrap UI Kit featuring over 80 premium components and 4 example pages.">
-<meta property="twitter:image" content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/pixel-lite/pixel-lite-preview.jpg">
 
 <!-- Favicon -->
 <link rel="apple-touch-icon" sizes="120x120" href="/assets/img/favicon/apple-touch-icon.png">
