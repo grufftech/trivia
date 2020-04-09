c57b611e514c2564658523a3c00ac77969ebd59f
Adam Baird <adam.baird@gmail.com>
2020-04-09 13:43:39 -0700

    updating base config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://trivia.test'),
+    'url' => env('APP_URL', 'https://trivia.gruff.tech/'),
 
     'asset_url' => env('ASSET_URL', null),
 
